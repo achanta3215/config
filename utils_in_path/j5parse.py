a438313afab98026d2b5b1f66204018601c53ec0
Krishna Sameer Achanta <achanta.ks@practo.com>
2020-01-04 14:13:23 +0530

    Format jsonoutput and indent according to indentation present in input

--- utils_in_path/j5parse.py
+++ utils_in_path/j5parse.py
@@ -5,6 +5,8 @@ print(end='')
 lines = sys.stdin.readlines()
 inputStr  = ''.join(str(s) for s in lines)
 
+toIndent = len(lines[1]) - len(lines[1].lstrip())
+
 startIndex = inputStr.find('{')
 endIndex = inputStr.rfind('}') + 1
 extractedStr = inputStr[startIndex:endIndex]
@@ -12,5 +14,10 @@ leftTruncatedString = inputStr[0:startIndex]
 rigthTruncatedString = inputStr[endIndex:len(inputStr) + 1]
 
 json5Output = json5.loads(extractedStr)
-jsonOutput = json5.dumps(json5Output, sort_keys=True, indent=4)
-print(leftTruncatedString + jsonOutput + rigthTruncatedString)
+jsonOutput = json5.dumps(json5Output, sort_keys=True, indent=toIndent)
+
+formattedJSONOutput = jsonOutput
+if (extractedStr.find("'") != -1):
+  formattedJSONOutput = jsonOutput.replace('"', "'")
+
+print(leftTruncatedString + formattedJSONOutput + rigthTruncatedString)
